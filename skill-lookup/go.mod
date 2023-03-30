392e48af3c894ffa5c52e110de22210839bef2e9
Daniel Vaughan <github@danielvaughan.com>
2023-03-30 10:33:45 +0100

    added better logging, sigterm handler, readiness and health checks

--- skill-lookup/go.mod
+++ skill-lookup/go.mod
@@ -22,6 +22,7 @@ require (
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.2.1 // indirect
 	github.com/googleapis/gax-go/v2 v2.7.0 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/kr/pretty v0.3.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.9.0 // indirect
--- skill-lookup/main.go
+++ skill-lookup/main.go
@@ -2,17 +2,24 @@ package main
 
 import (
 	"bufio"
+	"cloud.google.com/go/compute/metadata"
 	"cloud.google.com/go/logging"
 	"cloud.google.com/go/storage"
 	"context"
 	_ "embed"
 	"encoding/json"
 	"fmt"
-	"html/template"
+	"github.com/gorilla/mux"
+	"google.golang.org/api/option"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"skillsmapper.org/skill-lookup/internal/skill/autocomplete"
 	"skillsmapper.org/skill-lookup/internal/util"
+	"sync/atomic"
 	"time"
 )
 
@@ -21,26 +28,41 @@ type autocompleteResponse struct {
 }
 
 var (
-	//go:embed template/template.html
-	indexTemplate string
 	bucketName    string
 	objectName    string
 	logger        *logging.Logger
 	storageClient *storage.Client
 )
 
+type App struct {
+	*http.Server
+}
+
 func init() {
 	bucketName = util.MustGetenv("BUCKET_NAME")
 	objectName = util.MustGetenv("OBJECT_NAME")
-	projectID := util.MustGetenv("PROJECT_ID")
 	serviceName := util.MustGetenv("SERVICE_NAME")
 
 	ctx := context.Background()
-	loggingClient, err := logging.NewClient(ctx, projectID)
+
+	projectID := os.Getenv("PROJECT_ID")
+	if projectID == "" {
+		projID, err := metadata.ProjectID()
+		if err != nil {
+			log.Fatalf("unable to detect Project ID from PROJECT_ID or metadata server: %v", err)
+		}
+		projectID = projID
+	}
+
+	loggingClient, err := logging.NewClient(ctx, projectID,
+		option.WithoutAuthentication(),
+		option.WithGRPCDialOption(
+			grpc.WithTransportCredentials(insecure.NewCredentials()),
+		))
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	logger = loggingClient.Logger(serviceName)
+	logger = loggingClient.Logger(serviceName, logging.RedirectAsJSON(os.Stderr))
 
 	storageClient, err = storage.NewClient(ctx)
 	if err != nil {
@@ -53,24 +75,25 @@ func init() {
 
 func main() {
 	trie := autocomplete.New()
-	populate(*trie, bucketName, objectName)
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// Parse the template file
-		tmpl, err := template.New("index").Parse(indexTemplate)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	server := &http.Server{
+		Addr: ":" + port,
+		// Add some defaults, should be changed to suit your use case.
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	r := mux.NewRouter()
 
-		// Execute the template and write the result to the response
-		err = tmpl.Execute(w, nil)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-	})
-	http.HandleFunc("/autocomplete", func(w http.ResponseWriter, r *http.Request) {
+	isReady := &atomic.Value{}
+	isReady.Store(false)
+	r.HandleFunc("/liveness", liveness)
+	r.HandleFunc("/readiness", readiness(isReady))
+	r.HandleFunc("/autocomplete", func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 		// Get the prefix from the query string
 		prefix := r.URL.Query().Get("prefix")
@@ -96,8 +119,43 @@ func main() {
 		w.Header().Set("Content-Type", "application/json")
 		w.Write(json)
 	})
+	server.Handler = r
 
-	http.ListenAndServe(":8080", nil)
+	logger.Log(logging.Entry{
+		Severity: logging.Info,
+		Payload:  "starting HTTP server"})
+
+	populate(*trie, bucketName, objectName)
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Server closed: %v", err)
+		}
+	}()
+	isReady.Store(true)
+
+	logger.Log(logging.Entry{
+		Severity: logging.Info,
+		Payload:  "service is ready"})
+
+	ctx := context.Background()
+	// Listen for SIGINT to gracefully shutdown.
+	nctx, stop := signal.NotifyContext(ctx, os.Interrupt, os.Kill)
+	defer stop()
+	<-nctx.Done()
+	logger.Log(logging.Entry{
+		Severity: logging.Info,
+		Payload:  "shutdown initiated"})
+
+	// Cloud Run gives apps 10 seconds to shutdown. See
+	// https://cloud.google.com/blog/topics/developers-practitioners/graceful-shutdowns-cloud-run-deep-dive
+	// for more details.
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+	server.Shutdown(ctx)
+	logger.Log(logging.Entry{
+		Severity: logging.Info,
+		Payload:  "shutdown complete"})
 }
 
 func populate(trie autocomplete.Trie, bucketName string, objectName string) {
@@ -110,6 +168,10 @@ func populate(trie autocomplete.Trie, bucketName string, objectName string) {
 	bucket := storageClient.Bucket(bucketName)
 	// Get a handle to the object
 	object := bucket.Object(objectName)
+
+	logger.Log(logging.Entry{
+		Severity: logging.Info,
+		Payload:  fmt.Sprintf("loading tags from %s", objectName)})
 
 	// Read the contents of the object
 	reader, err := object.NewReader(context.Background())
@@ -137,12 +199,26 @@ func populate(trie autocomplete.Trie, bucketName string, objectName string) {
 		return
 	}
 
-	duration := time.Since(start)
+	duration := time.Since(start).Seconds()
 	logger.Log(logging.Entry{
 		Severity: logging.Debug,
-		Payload:  fmt.Sprintf("populate took %v", duration)})
+		Payload:  fmt.Sprintf("populate tags took %.2fs", duration)})
 
 	logger.Log(logging.Entry{
 		Severity: logging.Info,
-		Payload:  fmt.Sprintf("%v tags loaded\n", count)})
+		Payload:  fmt.Sprintf("loaded %d tags", count)})
+}
+
+func readiness(isReady *atomic.Value) http.HandlerFunc {
+	return func(w http.ResponseWriter, _ *http.Request) {
+		if isReady == nil || !isReady.Load().(bool) {
+			http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+	}
+}
+
+func liveness(w http.ResponseWriter, _ *http.Request) {
+	w.WriteHeader(http.StatusOK)
 }
