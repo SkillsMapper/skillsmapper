078ffea169f721a792d79cf71755d90f935b61b5
Daniel Vaughan <github@danielvaughan.com>
2023-04-10 15:31:16 +0100

    added diagram

--- user-interface/chapter/link.sh
+++ user-interface/chapter/link.sh
@@ -4,3 +4,4 @@ ln -s /Users/danielvaughan/Development/git/book/cloud-native-development-with-go
 ln -s /Users/danielvaughan/Development/git/book/cloud-native-development-with-google-cloud/images/chapter-6-neg.png images/chapter-6-neg.png
 ln -s /Users/danielvaughan/Development/git/book/cloud-native-development-with-google-cloud/images/chapter-6-proxy.png images/chapter-6-proxy.png
 ln -s /Users/danielvaughan/Development/git/book/cloud-native-development-with-google-cloud/images/chapter-6-url-map.png images/chapter-6-url-map.png
+ln -s /Users/danielvaughan/Development/git/book/cloud-native-development-with-google-cloud/images/chapter-6-solution.png images/chapter-6-solution.png
