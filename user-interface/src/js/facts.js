26fa0f6ccac920bf9e01eb0f79f9302aa6c18c62
Daniel Vaughan <github@danielvaughan.com>
2023-04-24 14:14:14 +0100

    cleaned up the facts UI

--- user-interface/src/js/facts.js
+++ user-interface/src/js/facts.js
@@ -2,34 +2,64 @@
 const FACTS_ENDPOINT = "/api/facts";
 
 async function fetchFacts() {
-    const token = await firebase.auth().currentUser.getIdToken();
-    console.log("Fetching facts...");
-    const response = await fetch(FACTS_ENDPOINT, {
-        headers: {
-            "Content-Type": "application/json",
-            'Authorization': `Bearer ${token}`
-        }
-    });
-    const data = await response.json();
-
-    const factsCollection = $("#factsCollection");
-    factsCollection.empty();
-    factsCollection.hide();
-    if (data._embedded && data._embedded.factList) {
-        factsCollection.show();
-        data._embedded.factList.forEach(fact => {
-            $("#factsCollection").append(
-                `<li class="collection-item">
-                   <span class="title">${fact.skill}</span>
-                    <span class="secondary-content delete">
-                      <button class="delete-btn" data-id="${fact.id}" style="background: none; border: none; padding: 0; cursor: pointer;">
-                        <i class="material-icons" style="cursor: pointer;">delete</i>
-                      </button>
-                    </span>
-                 </li>`);
+    try {
+        $("#error-message-404").hide();
+        $("#error-message-504").hide();
+        $("#progress-bar").show();
+        const token = await firebase.auth().currentUser.getIdToken();
+        console.log("Fetching facts...");
+        const response = await fetch(FACTS_ENDPOINT, {
+            headers: {
+                "Content-Type": "application/json",
+                'Authorization': `Bearer ${token}`
+            }
         });
-    } else {
-        console.log("No _embedded or factList found in the response data.");
+
+        if (response.status === 504) {
+            throw new Error("504 Gateway Timeout");
+        } else if (response.status === 404) {
+            throw new Error("404 Not Found");
+        }
+
+        const data = await response.json();
+        $("#progress-bar").hide();
+
+        const factsCollection = $("#factsCollection");
+        factsCollection.empty();
+        factsCollection.hide();
+        if (data._embedded && data._embedded.factList) {
+            factsCollection.show();
+            data._embedded.factList.forEach(fact => {
+                $("#factsCollection").append(
+                    `<li class="collection-item">
+                        <span class="chip chip-fixed-width ${
+                                            fact.level === 'interested' ? 'chip-interested' :
+                                                fact.level === 'learning' ? 'chip-learning' :
+                                                    fact.level === 'using' ? 'chip-using' :
+                                                        fact.level === 'used' ? 'chip-used' : ''
+                                        }">${fact.level}</span>
+                        <span class="title">${fact.skill}</span>
+                        <span class="secondary-content delete">
+                            <button class="delete-btn" data-id="${fact.id}" style="background: none; border: none; padding: 0; cursor: pointer;">
+                                <i class="material-icons" style="cursor: pointer;">delete</i>
+                            </button>
+                        </span>
+                    </li>`);
+            });
+        } else {
+            console.log("No _embedded or factList found in the response data.");
+        }
+    } catch (error) {
+        if (error.message === "504 Gateway Timeout") {
+            $("#progress-bar").hide();
+            $("#error-message-504").show();
+            setTimeout(fetchFacts, 60000); // Retry after 1 minute
+        } else if (error.message === "404 Not Found") {
+            $("#progress-bar").hide();
+            $("#error-message-404").show();
+        } else {
+            console.error("Error fetching facts:", error);
+        }
     }
 }
 
@@ -71,7 +101,6 @@ $('#addFact').click(async function () {
     const level = levelField.val();
     if (skill && level) {
         const result = await submitFact(skill, level);
-
         if (result) {
             M.toast({html: "Fact submitted successfully"}, 3000);
             console.info("Fact submitted successfully", result)
