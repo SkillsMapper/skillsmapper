0b4ffc7b263ea889c7a35eeb770ae3520cf9effd
Daniel Vaughan <github@danielvaughan.com>
2023-04-27 15:18:42 +0100

    reorganised projects and chapters to match

--- link.sh
+++ link.sh
@@ -15,12 +15,14 @@ ln -s -f /Users/danielvaughan/Development/git/book/cloud-native-development-with
 ln -s -f /Users/danielvaughan/Development/git/book/cloud-native-development-with-google-cloud/ch13.asciidoc chapters/ch13.asciidoc
 ln -s -f /Users/danielvaughan/Development/git/book/cloud-native-development-with-google-cloud/ch14.asciidoc chapters/ch14.asciidoc
 
+ln -s -f /Users/danielvaughan/Development/git/book/cloud-native-development-with-google-cloud/ch05.asciidoc laboratory/chapter/ch05.asciidoc
 ln -s -f /Users/danielvaughan/Development/git/book/cloud-native-development-with-google-cloud/ch06.asciidoc tag-updater/chapter/ch06.asciidoc
 ln -s -f /Users/danielvaughan/Development/git/book/cloud-native-development-with-google-cloud/ch07.asciidoc skill-lookup/chapter/ch07.asciidoc
 ln -s -f /Users/danielvaughan/Development/git/book/cloud-native-development-with-google-cloud/ch08a.asciidoc fact-service-native/chapter/ch08a.asciidoc
 ln -s -f /Users/danielvaughan/Development/git/book/cloud-native-development-with-google-cloud/ch08b.asciidoc fact-service-agnostic/chapter/ch08b.asciidoc
 ln -s -f /Users/danielvaughan/Development/git/book/cloud-native-development-with-google-cloud/ch09.asciidoc profile-builder/chapter/ch09.asciidoc
 ln -s -f /Users/danielvaughan/Development/git/book/cloud-native-development-with-google-cloud/ch10.asciidoc user-interface/chapter/ch10.asciidoc
+ln -s -f /Users/danielvaughan/Development/git/book/cloud-native-development-with-google-cloud/ch11.asciidoc factory/chapter/ch10.asciidoc
 
 ln -s -f /Users/danielvaughan/Development/git/book/cloud-native-development-with-google-cloud/ch05.asciidoc laboratory/chapter/ch05.asciidoc
 ln -s -f /Users/danielvaughan/Development/git/book/gcp-book/src/technologies.md chapters/technologies.md
