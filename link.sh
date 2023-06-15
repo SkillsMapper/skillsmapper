bc5c8843471c6f3283d65c26b9c16b2446f9c812
Daniel Vaughan <github@danielvaughan.com>
2023-06-15 07:21:23 +0100

    added log based metrics

--- link.sh
+++ link.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# Chapters
 ln -s -f /Users/danielvaughan/Development/git/book/cloud-native-development-with-google-cloud/ch01.asciidoc chapters/ch01.asciidoc
 ln -s -f /Users/danielvaughan/Development/git/book/cloud-native-development-with-google-cloud/ch02.asciidoc chapters/ch02.asciidoc
 ln -s -f /Users/danielvaughan/Development/git/book/cloud-native-development-with-google-cloud/ch03.asciidoc chapters/ch03.asciidoc
@@ -15,6 +17,7 @@ ln -s -f /Users/danielvaughan/Development/git/book/cloud-native-development-with
 ln -s -f /Users/danielvaughan/Development/git/book/cloud-native-development-with-google-cloud/ch13.asciidoc chapters/ch13.asciidoc
 ln -s -f /Users/danielvaughan/Development/git/book/cloud-native-development-with-google-cloud/ch14.asciidoc chapters/ch14.asciidoc
 
+# Chapters in Projects
 ln -s -f /Users/danielvaughan/Development/git/book/cloud-native-development-with-google-cloud/ch04.asciidoc setup/chapter/ch04.asciidoc
 ln -s -f /Users/danielvaughan/Development/git/book/cloud-native-development-with-google-cloud/ch05.asciidoc laboratory/chapter/ch05.asciidoc
 ln -s -f /Users/danielvaughan/Development/git/book/cloud-native-development-with-google-cloud/ch06.asciidoc tag-updater/chapter/ch06.asciidoc
@@ -25,21 +28,21 @@ ln -s -f /Users/danielvaughan/Development/git/book/cloud-native-development-with
 ln -s -f /Users/danielvaughan/Development/git/book/cloud-native-development-with-google-cloud/ch10.asciidoc user-interface/chapter/ch10.asciidoc
 ln -s -f /Users/danielvaughan/Development/git/book/cloud-native-development-with-google-cloud/ch11.asciidoc factory/chapter/ch11.asciidoc
 ln -s -f /Users/danielvaughan/Development/git/book/cloud-native-development-with-google-cloud/ch12.asciidoc observatory/chapter/ch12.asciidoc
+ln -s -f /Users/danielvaughan/Development/git/book/cloud-native-development-with-google-cloud/ch13.asciidoc scaling/chapter/ch13.asciidoc
+ln -s -f /Users/danielvaughan/Development/git/book/cloud-native-development-with-google-cloud/ch14.asciidoc further/chapter/ch14.asciidoc
 
-ln -s -f /Users/danielvaughan/Development/git/book/cloud-native-development-with-google-cloud/ch05.asciidoc laboratory/chapter/ch05.asciidoc
+# Tracking
 ln -s -f /Users/danielvaughan/Development/git/book/gcp-book/src/technologies.md chapters/technologies.md
-
-ln -s -f /Users/danielvaughan/Development/git/book/cloud-native-development-with-google-cloud/images/cloud-sql-proxy.png chapters/images/cloud-sql-proxy.png
-
 ln -s -f /Users/danielvaughan/Development/git/book/gcp-book/src/project-structure.md chapters/project-structure.md
 
+# Images
+ln -s -f /Users/danielvaughan/Development/git/book/cloud-native-development-with-google-cloud/images/cloud-sql-proxy.png chapters/images/cloud-sql-proxy.png
 ln -s -f /Users/danielvaughan/Development/git/book/cloud-native-development-with-google-cloud/images/p1-postit.png chapters/images/p1-postit.png
 ln -s -f /Users/danielvaughan/Development/git/book/cloud-native-development-with-google-cloud/images/p2-postit.png chapters/images/p2-postit.png
 ln -s -f /Users/danielvaughan/Development/git/book/cloud-native-development-with-google-cloud/images/p3-postit.png chapters/images/p3-postit.png
 ln -s -f /Users/danielvaughan/Development/git/book/cloud-native-development-with-google-cloud/images/p4-postit.png chapters/images/p4-postit.png
 ln -s -f /Users/danielvaughan/Development/git/book/cloud-native-development-with-google-cloud/images/p5-postit.png chapters/images/p5-postit.png
 ln -s -f /Users/danielvaughan/Development/git/book/cloud-native-development-with-google-cloud/images/p6-postit.png chapters/images/p6-postit.png
-
 ln -s -f /Users/danielvaughan/Development/git/book/cloud-native-development-with-google-cloud/images/factory.png chapters/images/factory.png
 ln -s -f /Users/danielvaughan/Development/git/book/cloud-native-development-with-google-cloud/images/continuous-integration.png chapters/images/continuous-integration.png
 ln -s -f /Users/danielvaughan/Development/git/book/cloud-native-development-with-google-cloud/images/continuous-deployment.png chapters/images/continuous-deployment.png
